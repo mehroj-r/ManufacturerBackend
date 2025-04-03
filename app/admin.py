95105b2da36cb010997eb36ce8cf5443e4f3aae4
MEHROJ <mekhrojmajidov@gmail.com>
2025-04-03 10:57:56 +0500

    All Models implemented

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Material, ProductMaterial, Warehouse
+
+admin.site.register(Product)
+admin.site.register(Material)
+admin.site.register(ProductMaterial)
+admin.site.register(Warehouse)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """
+    Model representing a product.
+    """
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Material(models.Model):
+    """
+    Model representing a material for a product.
+    """
+    name = models.CharField(max_length=255)
+    product = models.ManyToManyField(Product, related_name='materials', through='ProductMaterial')
+
+    def __str__(self):
+        return self.name
+
+class ProductMaterial(models.Model):
+    """
+    Model representing the relationship between a product and its materials.
+    """
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+
+    class Meta:
+        unique_together = ('product', 'material')
+
+    def __str__(self):
+        return f"{self.product.name} - {self.material.name} ({self.quantity})"
+
+class Warehouse(models.Model):
+    """
+    Model representing a warehouse.
+    """
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    remainder = models.FloatField()
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.material.name} - {self.remainder} units at price of {self.price} each"
